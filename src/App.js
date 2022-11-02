360e0e311223308b2e260f7c35135df0d7522c24
Yunos_T <yunoshevv2000@gmail.com>
2022-11-02 16:06:53 +0200

    add start page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { PureComponent } from 'react'
-import './style/App.scss'
+import React, { PureComponent } from 'react';
+import './style/App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <>
+        <div className="start_content">
+          <img src="/image/intro-page.png" alt="intro-page" />
+          <div className="content_title">
+            <h1>Quizzical</h1>
+            <button className="start_btn">Start quiz</button>
+          </div>
+        </div>
+      </>
     </div>
   );
 }
